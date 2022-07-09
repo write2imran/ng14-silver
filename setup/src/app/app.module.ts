9d1214b9afcd3a79be6774f5b55a5b08a1e3f96f
Imran Rauf <write2imran@yahoo.com>
2022-07-09 04:17:15 +0300

    material libraries configured

--- setup/src/app/app.module.ts
+++ setup/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    MatSliderModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
