194d01684581956b7d06837552d068ab2c16d404
Imran Rauf <write2imran@yahoo.com>
2022-07-09 04:55:01 +0300

    Schematic & DevTools installed with Root State

--- setup/src/app/app.module.ts
+++ setup/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, 
-    MatSliderModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})
+    BrowserModule,
+    MatSliderModule, BrowserAnimationsModule, StoreModule.forRoot({}, {}), StoreModule.forRoot(reducers, { metaReducers }), !environment.production ? StoreDevtoolsModule.instrument() : [], StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
